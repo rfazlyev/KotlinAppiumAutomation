97cdfafb799b13bba0abab3a008c2912ed0072d7
Fazlyev <ruslan.fazlyev@indriver.com>
2022-08-18 17:08:54 +0300

    Сделал ДЗ-2

--- KotlinAppiumAutomation/KotlinAppiumAutomation/src/test/kotlin/FirstTest.kt
+++ KotlinAppiumAutomation/KotlinAppiumAutomation/src/test/kotlin/FirstTest.kt
@@ -1,2 +1,42 @@
+import io.appium.java_client.AppiumDriver
+import io.appium.java_client.android.AndroidDriver
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.remote.DesiredCapabilities
+import java.net.URL
+
 class FirstTest {
+
+    private lateinit var driver: AppiumDriver<WebElement>
+
+    @Before
+    fun setUp() {
+
+        val capabilities = DesiredCapabilities()
+
+        capabilities.setCapability("platformName", "Android")
+        capabilities.setCapability("deviceName", "AndroidTestDevice")
+        capabilities.setCapability("platformVersion", "8.0")
+        capabilities.setCapability("automationName", "Appium")
+        capabilities.setCapability("appPackage", "org.wikipedia")
+        capabilities.setCapability("appActivity", ".main.MainActivity")
+        capabilities.setCapability(
+            "app",
+            "/Users/indriver/Desktop/KotlinAppiumAutomation/KotlinAppiumAutomation/apks/org.wikipedia.apk"
+        )
+
+        driver = AndroidDriver(URL("http://127.0.0.1:4723/wd/hub"), capabilities)
+    }
+
+    @After
+    fun tearDown() {
+        driver.quit()
+    }
+
+    @Test
+    fun firstTest() {
+        println("First test run")
+    }
 }
