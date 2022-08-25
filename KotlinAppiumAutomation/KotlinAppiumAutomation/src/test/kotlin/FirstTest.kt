c1536a134f70e9a6d034d0ae20f5e807404b904b
Fazlyev <ruslan.fazlyev@indriver.com>
2022-08-25 16:56:40 +0300

    Сделал первое задание третьего урока

--- KotlinAppiumAutomation/KotlinAppiumAutomation/src/test/kotlin/FirstTest.kt
+++ KotlinAppiumAutomation/KotlinAppiumAutomation/src/test/kotlin/FirstTest.kt
@@ -1,10 +1,14 @@
 import io.appium.java_client.AppiumDriver
 import io.appium.java_client.android.AndroidDriver
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.openqa.selenium.By
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
 import java.net.URL
 
 class FirstTest {
@@ -36,7 +40,55 @@ class FirstTest {
     }
 
     @Test
-    fun firstTest() {
-        println("First test run")
+    fun checkTextInInputField() {
+
+        assertElementHasText(
+            By.xpath("//*[@resource-id='org.wikipedia:id/search_container']//*[@text='Search Wikipedia']"),
+            "Search Wikipedia",
+            "Expected text not found"
+        )
+    }
+
+    fun assertElementHasText(by: By, title: String, errorMessage: String) {
+
+        val element: WebElement = waitForElementPresent(by, errorMessage, 10)
+
+        val textFromElement = element.text
+
+        Assert.assertEquals(errorMessage, title, textFromElement)
+    }
+
+    fun waitForElementPresent(by: By, errorMessage: String, timeoutInSeconds: Long): WebElement {
+
+        val wait = WebDriverWait(driver, timeoutInSeconds)
+
+        wait.withMessage(errorMessage + "\n")
+
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by))
+    }
+    fun waitForElementNotPresent(by: By, errorMessage: String, timeoutInSeconds: Long): Boolean {
+
+        val wait = WebDriverWait(driver, timeoutInSeconds)
+
+        wait.withMessage(errorMessage + "\n")
+
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by))
+    }
+    fun waitForElementAndClick(by: By, errorMessage: String, timeoutInSeconds: Long): WebElement {
+
+        val element: WebElement = waitForElementPresent(by, errorMessage, timeoutInSeconds)
+
+        element.click()
+
+        return element
+    }
+
+    fun waitForElementAndSendKeys(by: By, value: String, errorMessage: String, timeoutInSeconds: Long): WebElement {
+
+        val element: WebElement = waitForElementPresent(by, errorMessage, timeoutInSeconds)
+
+        element.sendKeys(value)
+
+        return element
     }
 }
